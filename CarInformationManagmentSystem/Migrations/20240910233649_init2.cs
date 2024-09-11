db846dbb5f226aaa91a124b9b3fc5c651540ffd1
vidyasagar Gajivelli <vidya-sagar.gajivelli@capgemini.com>
2024-09-11 13:13:39 +0530

    added git ignore. good to go.

---
+++ CarInformationManagmentSystem/Migrations/20240910233649_init2.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarInformationManagmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_ManufacturerId",
+                table: "Cars",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_TransmissionId",
+                table: "Cars",
+                column: "TransmissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_TypeId",
+                table: "Cars",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_CarTransmissionTypes_TransmissionId",
+                table: "Cars",
+                column: "TransmissionId",
+                principalTable: "CarTransmissionTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_CarTypes_TypeId",
+                table: "Cars",
+                column: "TypeId",
+                principalTable: "CarTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Manufacturers_ManufacturerId",
+                table: "Cars",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_CarTransmissionTypes_TransmissionId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_CarTypes_TypeId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Manufacturers_ManufacturerId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_ManufacturerId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_TransmissionId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_TypeId",
+                table: "Cars");
+        }
+    }
+}
