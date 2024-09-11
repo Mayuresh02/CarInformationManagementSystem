5efffe5c19676aa694c34d3d6e0e47c7aa3b2d2c
vidyasagar Gajivelli <vidya-sagar.gajivelli@capgemini.com>
2024-09-11 13:44:47 +0530

    Sagar

--- CarInformationManagmentSystem/obj/Debug/net8.0/CarInformationManagmentSystem.AssemblyInfo.cs
+++ CarInformationManagmentSystem/obj/Debug/net8.0/CarInformationManagmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInformationManagmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db846dbb5f226aaa91a124b9b3fc5c651540ffd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInformationManagmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInformationManagmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
